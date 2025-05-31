9d61bed98bc8d72124b963cf8be3b1d74f20f1f1
Taninwat-55 <60184555+Taninwat-55@users.noreply.github.com>
2025-05-31 21:36:49 +0200

    just add thing in env file

--- frontend/src/pages/NewThreadPage.jsx
+++ frontend/src/pages/NewThreadPage.jsx
@@ -3,7 +3,6 @@ import ThreadForm from '../components/ThreadForm';
 function NewThreadPage() {
   return (
     <div className="new-thread-page">
-      <h2>Create New Thread</h2>
       <ThreadForm />
     </div>
   );
